8b31f3d89c0f1090455c5117b420b7e66ad4261b
Maksymilian Sobiech <M.S.Sobiech@student.tudelft.nl>
2024-11-23 13:27:50 +0100

    fix: Removed unused imports

--- client/src/main/java/client/scenes/NoteOverviewCtrl.java
+++ client/src/main/java/client/scenes/NoteOverviewCtrl.java
@@ -21,8 +21,6 @@ import java.util.ResourceBundle;
 import com.google.inject.Inject;
 
 import client.utils.ServerUtils;
-import commons.Quote;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
