32b023d853087a8bf2745858a275652a28faa9a5
Lapo Masiero <l.p.r.masiero@student.tudelft.nl>
2024-12-09 01:15:08 +0100

    fix: fixed pipeline maybe pt.2

--- client/src/test/java/client/scenes/ErrorPopUpCtrlTest.java
+++ client/src/test/java/client/scenes/ErrorPopUpCtrlTest.java
@@ -5,13 +5,17 @@ import client.controllers.ErrorPopUpCtrl;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testfx.assertions.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class ErrorPopUpCtrlTest{
@@ -19,14 +23,26 @@ public class ErrorPopUpCtrlTest{
     @InjectMocks
     ErrorPopUpCtrl errorCtrl;
 
+
+    @BeforeEach
+    public void setUpHeadlessMode() {
+        System.setProperty("java.awt.headless", "true");
+    }
+
     @BeforeAll
     public static void setUp(){
-        System.setProperty("java.awt.headless", "true");
         Platform.startup(()->{});
+    }
+
+
+    @Test
+    public void whenSetUpSuccessful_thenHeadlessIsTrue() {
+        assertThat(GraphicsEnvironment.isHeadless()).isTrue();
     }
 
     @Test
     public void setLabelTest(){
+
         assertEquals(errorCtrl.setErrorLabel("error"), "Oh no! It seems the following error occurred:\nerror"+
             "\nFor common fixes please visit: \nhttps://emmer.dev/blog/common-markdown-mistakes/");
     }
