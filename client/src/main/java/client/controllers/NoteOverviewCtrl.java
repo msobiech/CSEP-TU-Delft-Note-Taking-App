f2141d5fbd959af99201873714968f2441ea4cf3
Floris van Engelen <f.j.g.engelen@student.tudelft.nl>
2025-01-13 16:02:33 +0100

    fixed build (unused imports)

--- client/src/main/java/client/controllers/NoteOverviewCtrl.java
+++ client/src/main/java/client/controllers/NoteOverviewCtrl.java
@@ -2,7 +2,6 @@ package client.controllers;
 
 
 import client.DialogFactory;
-import client.InjectorProvider;
 import client.event.*;
 import client.managers.LanguageManager;
 import client.managers.MarkdownRenderManager;
@@ -12,7 +11,6 @@ import client.utils.DebounceService;
 import client.utils.NoteService;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
